f1b84151ca1abc91bf9f023e34770e3be2eae955
Nitish Rai <158585222+Nitishnitpatna@users.noreply.github.com>
2024-10-03 06:05:19 +0000

    container config done

---
+++ 01_Basics/01_variabals.js
@@ -0,0 +1,12 @@
+const userId = 19
+let userEmail = "rs@google.com"
+var userName = "Rahul"
+userPassword = "921342"
+let userState;
+
+// userId = 11
+userEmail = "rahul@google"
+userName = "ravi"
+userPassword = "123456"
+// console.log(userId)
+console.table([userId,userEmail,userName,userPassword,userState])
