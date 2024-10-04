564dfce8de177d38a136410acf4c4369adf5803a
Nitish Rai <158585222+Nitishnitpatna@users.noreply.github.com>
2024-10-04 05:17:17 +0000

    Datatype

---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // It treat JS according to newer version
+
+// alert(3+3) // we are in nodejs not at browser
+
+/*
+For documentation use
+1 MDN
+2 TCMA (for standards of JS)
+*/
+
+let name = "Nitish Rai"
+let age = 19
+let isLogged = false
+
+// Number => 2 to power 53
+// bigint
+// boolean
+// string = ""
+/* null => *it is a standalone value intensionally bhi use hota h kabhi kabhi
+         *it is a typeof object   */
+         console.log(typeof null) //object
+         
+/* undefined => koi bhi value nhi hai waha pe
+               * it has a special type of undifined */
+              console.log(typeof undefined) //undefined
+// symbols => for uniqeness
